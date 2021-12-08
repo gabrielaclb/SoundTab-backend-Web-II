84d5a5a43ad32d03f75e7002f9bf1fd069ae9b38
Gabriela <gabrielaclb5@gmail.com>
2021-12-08 01:18:08 -0300

    Conexion a base de datos en la nube

--- src/config/db.js
+++ src/config/db.js
@@ -4,36 +4,36 @@ let connectionLimit,
     host,
     database,
     user,
-    password,
+    pa$$word,
     port
 
 switch(process.env.NODE_ENV) {
   default:
   case 'development':
-    connectionLimit = 10
-    host = 'localhost'
+    connectionLimit = 100
+    host = '64'+'.227'+'.12'+'.252' //Costumbre de seguridad :3
     port = 3306
     database = 'soundtab'
-    user = 'root'
-    password = 'p6X2FNsM_'
+    user = 'administrador'
+    pa$$word = 'p6X2'+'FNsM_'
     break
 
   case 'staging':
     connectionLimit = 50,
-    host = 'localhost'
+    host = '64'+'.227'+'.12'+'.252'
     port = 3306
     database = 'soundtab'
     user = 'administrador'
-    password = 'p6X2FNsM_'
+    pa$$word = 'p6X2F'+'NsM_'
     break
 
   case 'production':
     connectionLimit = 50
-    host = 'localhost'
+    host = '64'+'.227'+'.12'+'.252'
     port = 3306
     database = 'soundtab'
     user = 'administrador'
-    password = 'p6X2FNsM_'
+    pa$$word = 'p6X2FNs'+'M_'
     break;
 }
 
@@ -41,7 +41,7 @@ const Config = new Object({
   connectionLimit : connectionLimit,
   host: host,
   user: user,
-  password: password,
+  password: pa$$word,
   database: database,
   port
 })
